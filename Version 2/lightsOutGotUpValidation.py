89f2c57aee047b5fd1be5238c542fbb42843fcb5
Daniel Backhouse <31683053+danielbackhouse@users.noreply.github.com>
2019-05-03 14:02:54 -0700

    Create lightsOutGotUpValidation.py

---
+++ Version 2/lightsOutGotUpValidation.py
@@ -0,0 +1,6 @@
+""" Title: lightsOutGotUpValidation
+    Purpose: Compares the lights out times and got up times determined following
+    the MotionWatch 8 Protocol and the MotionWareAnalysisProgram and plots
+    the relative errors between the two
+    Author: Daniel Backhouse and Alan Yan
+"""
