0b0ceaca651aa8ec032cc5d109fabf1587cb955d
Daniel Backhouse <31683053+danielbackhouse@users.noreply.github.com>
2019-05-03 15:42:26 -0700

    Added get_sleep_analysis_times function to the error checking module in the repository (lightsOutGotUpValidation)

--- Version 2/lightsOutGotUpValidation.py
+++ Version 2/lightsOutGotUpValidation.py
@@ -4,12 +4,50 @@
     the relative errors between the two
     Author: Daniel Backhouse and Alan Yan
 """
-
+# Import extension libraries
 import MotionWareAnalysis
 import SheetManager
 import pandas as pd
 
-rawDataDirectory = r'C:\Users\dbackhou\Desktop\Buying Time Study Copy\BT Sleep Analysis 2019-03-19.xlsx'
+# Define global variables
+sleepAnalysisDirectory = r'C:\Users\dbackhou\Desktop\Buying Time Study Copy\BT Sleep Analysis 2019-03-19.xlsx'
+lightsOutIndex  = 2;
+getUpIndex  = 5;
+
+def get_sleep_analysis_times():
+    """Gets the lights out and got up times of the participant as determined in
+    in the sleep analysis
+    
+    :param: none
+    :return: Returns the lights out and got up tims of the participant as 
+        specified within the sleep analysis spreadsheet
+    :rtype: (list)
+    """
+    sleepAnalysis = pd.read_excel(sleepAnalysisDirectory, 
+                                  sheet_name = 'BT-001 Baseline', skiprows = 16)
+    
+    dates = list(sleepAnalysis)     #get the dates the study was done over
+    dates.pop()
+    dates.pop()
+    dates.reverse()
+    dates.pop()
+    dates.reverse()
+    
+    lightsOutAnalysis = list()
+    gotUpAnalysis = list()
+    
+    # iterate over study dates within excel sheet at specified indices
+    # to get the lights out and got up values for each specific date
+    for day in dates:
+        lightsOutTime = sleepAnalysis.get_value(lightsOutIndex, day)
+        getUpTime = sleepAnalysis.get_value(getUpIndex, day)
+        
+        lightsOutAnalysis.append(lightsOutTime)
+        gotUpAnalysis.append(getUpTime)
+    
+    return lightsOutAnalysis, gotUpAnalysis
+
+
 
 rawDataList = SheetManager.populateRawDataList()
 diaryList = SheetManager.populateDiaryList()
