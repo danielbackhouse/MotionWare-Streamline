a5fdb8be3511d7d13cae0e64b473ed22bc4aee74
Backhouse <dbackhou@ead.ubc.ca>
2019-05-07 10:23:40 -0700

    Update SheetManager.py

--- Version 2/SheetManager.py
+++ Version 2/SheetManager.py
@@ -12,6 +12,7 @@ import pandas as pd
 #TODO: changes the diretories to be in the RunMotionWareProcessing program
 rawDataDirectory = r'C:\Users\dbackhou\Desktop\Buying Time Study Copy\RAW data'
 sleepDiaryDirectory  = r'C:\Users\dbackhou\Desktop\Buying Time Study Copy\Edited Sleep Diary\BT Sleep Diary Edited.xlsx'
+origSleepDiaryDirectory  = r'C:\Users\dbackhou\Desktop\Buying Time Study Copy\Edited Sleep Diary\BT Sleep Diary.xlsx'
 diaryLightsOutIndex = 1;
 diaryGotUpIndex = 5;
 
@@ -64,20 +65,57 @@ def populateRawDataList():
 
 def populateDiaryList():
     """Creates and populates a list of all sleep diaries, in numerical order
-        
-    Looks at all         
+              
     :param: none
-    :return: list of all trials sleep diaries
+    :return: list of all trials sleep diaries and list of sheet names
     :rtype(list)
     """
     diaryList = []
     xlsx = pd.ExcelFile(sleepDiaryDirectory)
     rawSleepDiary = pd.ExcelFile(xlsx)
     # Added sheet name list
-    sheetNameList = list()
+    sheetNameList = []
     
     for sheetName in rawSleepDiary.sheet_names:
         if findMatching(sheetName):
             diaryList.append(pd.read_excel(sleepDiaryDirectory, sheet_name = sheetName))
             sheetNameList.append(sheetName)
     return diaryList, sheetNameList        
+
+
+def populateSplitDiaryLists():
+    """Creates and populates a list of finished and unfinished sleep diaries
+    in numerical order
+                 
+    :param: none
+    :return: 4 lists, filled sleep diaries, unfinished sleep diaries, their respective sheet names
+    :rtype(list)
+    """
+    filledDiaryList = []
+    incompleteDiaryList = []
+    xlsx = pd.ExcelFile(origSleepDiaryDirectory)
+    rawSleepDiary = pd.ExcelFile(xlsx)
+    # Added sheet name list
+    filledSheetNameList = []
+    incompleteSheetNameList = []
+    
+    for sheetName in rawSleepDiary.sheet_names:
+        if findMatching(sheetName):
+            currentSheet = pd.read_excel(origSleepDiaryDirectory, sheet_name = sheetName)
+            if currentSheet.iloc[1,1:15].isnull().sum() or currentSheet.iloc[5,1:15].isnull().sum():
+                incompleteDiaryList.append(currentSheet)
+                incompleteSheetNameList.append(sheetName)
+                print('incomplete: ' + sheetName)
+            else:
+                filledDiaryList.append(currentSheet)
+                filledSheetNameList.append(sheetName)   
+                
+    return filledDiaryList,incompleteDiaryList, filledSheetNameList, incompleteSheetNameList   
+
+
+
+
+
+
+
+ 
