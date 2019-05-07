60d55e9817b15111ee1dcfdd6d94771c892c9bc2
danielbackhouse <danielbackhouse49d@gmail.com>
2019-05-07 16:10:51 -0700

    Create thresholdOptimization.py

---
+++ Version 3/thresholdOptimization.py
@@ -0,0 +1,7 @@
+""" Title: thresholdOptimization
+    Purpose: To determine the ideal threshold values for the lights out
+    get up algorithm
+    Author: Daniel Backhouse and Alan Yan
+"""
+
+import numpy as np
