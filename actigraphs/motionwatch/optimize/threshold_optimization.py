45a2dc1509342bc20454dec63e3a7addc137b337
Daniel Backhouse <31683053+danielbackhouse@users.noreply.github.com>
2019-05-28 15:59:20 -0700

    some updates

--- actigraphs/motionwatch/optimize/threshold_optimization.py
+++ actigraphs/motionwatch/optimize/threshold_optimization.py
@@ -7,8 +7,8 @@ import time
 import numpy as np 
 
 def optimize_LO_times(sleep_study, LOprotocol):
-    ws = 10
-    dm = 30
+    ws = 4
+    dm = 4
     zmc = 45
     zac = 5
     zlc = 30
@@ -16,48 +16,19 @@ def optimize_LO_times(sleep_study, LOprotocol):
     
     error_averages = []
     std_study = []
-    while ws > 3:
-        print('count')
-        print(ws)
-        while dm > 3:
-            print('Optimizing... (kinda)')
-            start = time.time()
+    error_averages.append([])
+    std_study.append([])
+    ws = np.linspace(4,10,7)
+    dm = np.linspace(4,30,27)
+    WS, DM = np.meshgrid(ws, dm)    
+    
+    for rows in WS:
             LOdates, GUdates, SleepAnalysisInfo, participant_list = sleep_study.get_in_bed_times(
                     ws,dm,zmc, zac, zlc, ta)
             error_study, useful_participants = err.get_error_study(LOdates, LOprotocol, 
                                                                    participant_list)
-            error_averages.append(err.total_error(error_study))
-            std_study.append(np.std(list(error_study.values()), ddof = 1))
-            dm = dm - 1
-            end = time.time()
-            print('Time elapsed')
-            print(start-end)
-            print(dm)
-        dm = 30
-        ws = ws - 1
-    
-    return error_averages, std_study
+            error_averages.append[index](err.total_error(error_study))
+            std_study.append[index](np.std(list(error_study.values()), ddof = 1))
 
-def gradient_descent(X, max_iterations, learning_rate, study, protocol):
-    error = function(X, study, protocol)
     
-    for i in range(0, max_iterations):
-        print('Iteration number:')
-        print(i)
-
-        
-    return error, X
-
-def gradient(X, learning_rate, study, protocol):
-    Xahead = X+learning_rate    deltaX = Xahead - X
-    Yahead = function(X, study, protocol)
-    Y = function(X, study, protocol)
-    partial_derivatives = (Yahead - Y)/deltaX  #X will be made into a vector containing the weights
-    
-    return partial_derivatives
-    
-def function(X, study, protocol):
-    LO, GU, SI, PL = study.get_in_bed_times(X)
-    error_study, useful_participants = err.get_error_study(LO, protocol, PL)
-    Y = err.total_error(error_study)
-    return Y
+    return error_averages, std_study, WS, DM
--- actigraphs/motionwatch/run_sleep_analysis.py
+++ actigraphs/motionwatch/run_sleep_analysis.py
@@ -13,6 +13,9 @@ import create.Study as study
 import create.ProtocolSleepAnalysis as ps
 import compute.error_analysis as err
 import optimize.threshold_optimization as thresh
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
     
 
 raw_data_directory = r"C:\Users\dbackhou\Desktop\BT Sleep Copy\Baseline"
@@ -30,4 +33,13 @@ protocol = ps.ProtocolSleepAnalysis(sa_directory, sleep_study.participant_list,
                                     study_name, assesment)
 LOprotocol, GUprotocol = protocol.get_study_analysis_sleep_times()
 
-error_averages, std_study = thresh.optimize_LO_times(sleep_study, LOprotocol)
+error_averages, std_study, ws_vector, dm_vector = thresh.optimize_LO_times(sleep_study, LOprotocol)
+
+#X, Y = np.meshgrid(ws_vector, dm_vector)
+#Z = error_averages
+#fig = plt.figure()
+#ax = plt.axes(projection='3d')
+#ax.contour3D(X, Y, Z, 50, cmap='binary')
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#ax.set_zlabel('z');
