811d1574dc9d2427897f49fa19f9aab090dd596b
Daniel Backhouse <31683053+danielbackhouse@users.noreply.github.com>
2019-05-28 09:34:13 -0700

    Updates

--- actigraphs/motionwatch/optimize/threshold_optimization.py
+++ actigraphs/motionwatch/optimize/threshold_optimization.py
@@ -37,27 +37,3 @@ def optimize_LO_times(sleep_study, LOprotocol):
         ws = ws - 1
     
     return error_averages, std_study
-
-def gradient_descent(X, max_iterations, learning_rate, study, protocol):
-    error = function(X, study, protocol)
-    
-    for i in range(0, max_iterations):
-        print('Iteration number:')
-        print(i)
-
-        
-    return error, X
-
-def gradient(X, learning_rate, study, protocol):
-    Xahead = X+learning_rate    deltaX = Xahead - X
-    Yahead = function(X, study, protocol)
-    Y = function(X, study, protocol)
-    partial_derivatives = (Yahead - Y)/deltaX  #X will be made into a vector containing the weights
-    
-    return partial_derivatives
-    
-def function(X, study, protocol):
-    LO, GU, SI, PL = study.get_in_bed_times(X)
-    error_study, useful_participants = err.get_error_study(LO, protocol, PL)
-    Y = err.total_error(error_study)
-    return Y
