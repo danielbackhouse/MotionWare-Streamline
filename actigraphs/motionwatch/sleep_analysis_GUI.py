26b663e8fcd552caae85df626515eb680c8e4207
Daniel Backhouse <31683053+danielbackhouse@users.noreply.github.com>
2019-05-30 12:23:33 -0700

    Added some of right column

--- actigraphs/motionwatch/sleep_analysis_GUI.py
+++ actigraphs/motionwatch/sleep_analysis_GUI.py
@@ -10,8 +10,27 @@ import run_sleep_analysis as run
 
 window = Tk()
 window.title("MotionWare Data Analysis")
-window.geometry("500x450")
+window.geometry("1000x550")
 
+#INPUT
+
+spacing = Canvas(height = 10, width = 1000)
+spacing.grid(row = 0, column = 0, columnspan = 6)
+#Input Title
+input_label = Label(window, text = 'Input')
+font = ('helvetica', 20, 'bold')
+input_label.config(font  = font, pady = 0)
+input_label.grid(row = 1, column = 0, columnspan = 3, rowspan = 1)
+
+output_label = Label(window, text = 'Output')
+font = ('helvetica', 20, 'bold')
+output_label.config(font  = font, pady = 0)
+output_label.grid(row = 1, column = 3, columnspan = 3, rowspan = 1)
+
+canvas = Canvas(height = 1, width = 1000)
+canvas.create_line(100, 2, 400, 2)
+canvas.create_line(600, 2, 900, 2)
+canvas.grid(row=2, columnspan = 6, column = 0, rowspan = 1)
 #Open Raw Data Directory
 def openRawDataFile():
     filename = filedialog.askdirectory(initialdir = "/", title = 'Select folder')
@@ -21,9 +40,9 @@ raw_data_label = Label(window, text = 'Enter the Raw Data Folder Path:')
 raw_data_entry = Entry(window, width = 50)
 raw_data_entry.insert(0, r"C:\Users\dbackhou\Desktop\BT Sleep Copy\Baseline")
 openFileButton = Button(window, text = 'Open Folder', command = openRawDataFile)
-raw_data_label.grid(row = 0, column = 0, columnspan = 3)
-raw_data_entry.grid(row = 1, column = 0, columnspan = 2)
-openFileButton.grid(row = 1, column = 2)
+raw_data_label.grid(row = 3, column = 0, columnspan = 3)
+raw_data_entry.grid(row = 4, column = 0, columnspan = 2)
+openFileButton.grid(row = 4, column = 2)
 
 #Open Sleep Diary File
 def openSleepDiaryFile():
@@ -34,9 +53,9 @@ sleep_diary_label = Label(window, text = 'Enter the Sleep Diary File Path:')
 sleep_diary_entry = Entry(window, width = 50)
 sleep_diary_entry.insert(0, r"C:\Users\dbackhou\Desktop\BT Sleep Copy\BT Sleep Diary.xlsx")
 openNewFileButton = Button(window, text = 'Open File', command = openSleepDiaryFile)
-sleep_diary_label.grid(row = 2, column = 0, columnspan = 3)
-sleep_diary_entry.grid(row = 3, column = 0, columnspan = 2)
-openNewFileButton.grid(row = 3, column = 2)
+sleep_diary_label.grid(row = 5, column = 0, columnspan = 3)
+sleep_diary_entry.grid(row = 6, column = 0, columnspan = 2)
+openNewFileButton.grid(row = 6, column = 2)
 
 #Skipped Rows
 skipped_rows = StringVar(window)
@@ -45,32 +64,32 @@ numbers = []
 for x in range(100):
     numbers.append(x)
 skipped_rows_list = OptionMenu(window, skipped_rows, *numbers)
-skipped_rows_list.grid(row=5, column = 0, columnspan = 3)
+skipped_rows_list.grid(row = 8, column = 0, columnspan = 3)
 skipped_rows_label = Label(window, text = 'Enter the Number of Rows To Skip in the Raw Data:')
-skipped_rows_label.grid(row = 4, column = 0, columnspan = 3)
+skipped_rows_label.grid(row = 7, column = 0, columnspan = 3)
 
 
 #Name of Study
 name_study_label = Label(window, text = 'Enter the name of the study:')
 name_study_entry = Entry(window, width = 50, justify = 'center')
 name_study_entry.insert(0, 'BT')
-name_study_label.grid(row = 6, column = 0, columnspan = 3)
-name_study_entry.grid(row = 7, column = 0, columnspan = 3)
+name_study_label.grid(row = 9, column = 0, columnspan = 3)
+name_study_entry.grid(row = 10, column = 0, columnspan = 3)
 
 #Assessment 
 assess_label = Label(window, text = 'Enter the assessment of the study:')
 assess_entry = Entry(window, width = 50, justify = 'center')
 assess_entry.insert(0, 'Baseline')
-assess_label.grid(row = 8, column = 0, columnspan = 3)
-assess_entry.grid(row = 9, column = 0, columnspan = 3)
+assess_label.grid(row = 11, column = 0, columnspan = 3)
+assess_entry.grid(row = 12, column = 0, columnspan = 3)
 
 #Trim Type
 trim = StringVar(window)
 trim.set(2) # default value
 trim_list = OptionMenu(window, trim, 0, 1, 2)
-trim_list.grid(row=11, column = 0, columnspan = 3)
+trim_list.grid(row=14, column = 0, columnspan = 3)
 trim_label = Label(window, text = 'Enter trim type:')
-trim_label.grid(row = 10, column = 0, columnspan = 3)
+trim_label.grid(row = 13, column = 0, columnspan = 3)
 
 
 #Start Button
@@ -90,17 +109,17 @@ def startGoing():
     window.destroy()
     run.run_program(raw_data_path, skipped_rows_num, study_name, assess, trim_num, sleep_diary_path)
 goButton = Button(window, text = 'Start', command = startGoing, width = 20, bg = 'green')
-goButton.grid(row = 12, column = 2)
+goButton.grid(row = 15, column = 4)
 
 #Quit Button
 quitButton = Button(window, text = 'QUIT', command = window.destroy, width = 20, bg = 'red')
-quitButton.grid(row = 12, column = 0)
+quitButton.grid(row = 15, column = 1)
 
 
 
-for x in range(3):
+for x in range(6):
     window.columnconfigure(x, minsize = 500/3)
 for y in range(18):
     window.rowconfigure(y ,pad = 10)
-window.rowconfigure(12, pad = 40)
+window.rowconfigure(15, pad = 40)
 window.mainloop()
